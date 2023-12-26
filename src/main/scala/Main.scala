d0b9dc30f5c7ce07ef25360533f683b71a24347c
Johannes <johannes@modulize.io>
2023-12-26 15:09:28 +0100

    Dummy commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,4 @@
 import bfcompiler.cli.CliApp
-import bfcompiler.intermediate.{
-  IntermediateCompilationError,
-  IntermediateCompiler
-}
-import bfcompiler.interpreter.Interpreter
-import bfcompiler.lexer.{Lexer, LexerError}
-import bfcompiler.util.ErrorReporting
-
-import java.nio.file.Paths
 
 @main def main(args: String*): Unit =
   CliApp.run(args, sys.env)
